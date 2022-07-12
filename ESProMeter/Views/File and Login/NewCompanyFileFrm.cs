f6c492c7ac4584b40e66a883960ead7ac8978bbf
kyhout <99602152+kyhout@users.noreply.github.com>
2022-07-12 22:59:41 +0700

    Update 1207222300

---
+++ ESProMeter/Views/File and Login/NewCompanyFileFrm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ESProMeter.Views.File_and_Login
+{
+	public partial class NewCompanyFileFrm : Form
+	{
+		public NewCompanyFileFrm()
+		{
+			InitializeComponent();
+		}
+	}
+}
