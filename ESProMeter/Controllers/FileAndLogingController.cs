c48841da5c1329da6192431b50066c3bd4543727
kyhout <99602152+kyhout@users.noreply.github.com>
2022-07-14 14:17:24 +0700

    Update 1407221417

--- ESProMeter/Controllers/FileAndLogingController.cs
+++ ESProMeter/Controllers/FileAndLogingController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using ESProMeter.DataAccess;
 
 namespace ESProMeter.Controllers
 {
 	class FileAndLogingController
 	{
-		public static void setServerConfiguration()
+		public static DataTable getActivedCompanyList()
 		{
-			
+
 		}
+
 	}
 }
