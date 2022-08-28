882d05f0df0886f603b371995d3a1a5e67c2642f
MON MINH <holee_ham@hotmail.com>
2022-08-28 17:44:18 +0700

    Update SqlUtility.cs

--- ESProMeter/DataAccess/SqlUtility.cs
+++ ESProMeter/DataAccess/SqlUtility.cs
@@ -6,7 +6,6 @@ namespace ESProMeter.DataAccess
 {
     public class SqlUtility
     {
-        public List<string> Messages { get; private set; }
         private readonly DacDeployOptions _deployOptions;
         private DacServices dacServices=null;
         public SqlUtility()
@@ -22,27 +21,17 @@ namespace ESProMeter.DataAccess
                 DropDmlTriggersNotInSource = false,
                 DeployDatabaseInSingleUserMode = true
             };
-            Messages = new List<string>();
-
         }
 
         public void DeployDatabase(string databasePath,string dbname)
         {
             dacServices = new DacServices("Data Source=.;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;");
-            dacServices.ProgressChanged += DacServices_ProgressChanged;
             dacServices.Deploy(DacPackage.Load(databasePath), dbname, true, options: _deployOptions);
         }
-
         public void DeployDatabase(DacServices svr,string databasePath, string dbname)
         {
             svr.Deploy(DacPackage.Load(databasePath), dbname, true, options: _deployOptions);
         }
-        private void DacServices_ProgressChanged(object sender, DacProgressEventArgs e)
-        {
-            Messages.Add(e.Message);
-            
-        }
-
         public void ExportDacPac(string databasePath, string dbname)
         {
             //dacServices.Extract(databasePath, dbname);
